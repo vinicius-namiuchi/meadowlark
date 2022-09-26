7d36492c33f1c975479880a34a9e6e5c0cf7f3f1
Vinicius Silva <vinicius.namiuchi@gmail.com>
2022-09-26 14:53:07 -0300

    Adicionado página de concurso de fotos de férias

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,7 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const weatherMiddlware = require('./lib/middleware/weather')
 const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
 
 const app = express()
 
@@ -31,11 +32,26 @@ app.use(weatherMiddlware)
 app.get('/', handlers.home)
 app.get('/section-test', handlers.sectionTest)
 
+app.use(bodyParser.json())
+
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
 app.get('/newsletter-signup', handlers.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
 app.get('/sobre', handlers.sobre)
+
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+        if(err) return handlers.vacationPhotoContestProcessError(req, res, err.message)
+        console.log('got fields: ', fields)
+        console.log('and files: ', files)
+        handlers.vacationPhotoContestProcess(req, res, fields, files)
+    })
+})
 
 // página 404 personalizada
 app.use(handlers.notFound)
