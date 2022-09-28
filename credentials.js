ed0c7f6ea048a0427407726b48c242ad6e9507da
Vinícius Namiuchi <vinicius-namiuchi@users.noreply.github.com>
2022-09-28 14:31:12 -0300

    Delete credentials.js

--- credentials.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    cookieSecret: "Seu cookie secreto fica aqui",
-    sendgrid: {
-        user: 'apikey',
-        password: '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',
-    }
-}
